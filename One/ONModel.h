89b8b935acba4c8595dc879aeba060b15018feb0
Jason Schatz <schatz@gmail.com>
2014-06-07 14:30:42 -0700

    empty model

---
+++ One/ONModel.h
@@ -0,0 +1,16 @@
+//
+//  ONModel.h
+//  One
+//
+//  Created by Jason Schatz on 6/7/14.
+//  Copyright (c) 2014 Jason Schatz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ONModel : NSObject
+
+@property (readonly, nonatomic) NSInteger count;
+@property (nonatomic, strong) NSString* recipient;
+
+@end
---
+++ One/ONModel.m
@@ -0,0 +1,13 @@
+//
+//  ONModel.m
+//  One
+//
+//  Created by Jason Schatz on 6/7/14.
+//  Copyright (c) 2014 Jason Schatz. All rights reserved.
+//
+
+#import "ONModel.h"
+
+@implementation ONModel
+
+@end
